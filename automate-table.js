2944ed1c0cd2879e875e3ced51b60ecb7a3289dc
Carlos Baston <cbaston@centralrecovery.com>
2018-05-08 12:32:01 -0700

    test

--- automate-table.js
+++ automate-table.js
@@ -9,7 +9,7 @@ const config = require(__dirname + '/configuration/config.js');
  * 3. Create table if does not exists
  * 4. Seed the documents into the table - also seeding id to tie everything
  * 5. If no errors another asynchronous stream of functions creates the indexes
- * TODO: Need to eventually refactor the flow of this process
+ * TODO: Need to eventually refactor the flow of this process.
  * * @param data
  */
 let automate = function(data) {
