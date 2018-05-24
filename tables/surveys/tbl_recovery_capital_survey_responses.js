5c9f301af7891a0ee13a1cc23c9a9b252d7b50c0
Carlos Baston <cbaston@centralrecovery.com>
2018-05-24 10:40:26 -0700

    automate survey questions and survey responses

---
+++ tables/surveys/tbl_recovery_capital_survey_responses.js
@@ -0,0 +1,21 @@
+const tables = require('../../configuration/tables-config')
+
+// Seed data.
+const seeder = []
+
+// create seed data.
+tables.tbl_recovery_capital_survey_responses.types.forEach((type, i) => {
+ seeder.push({
+     "DESCRIPTION": type.DESCRIPTION,
+     "ID": i + 1,
+     "VISUAL": type.VISUAL
+ })
+})
+
+module.exports = {
+  "seeder": seeder,
+  "indexes": [],
+  "compoundIndexes": [],
+  "table": "TBL_RECOVERY_CAPITAL_SURVEY_RESPONSES"
+}
+
