c28de67462d6753891a5e613718d3b46b0ae60ce
Carlos Baston <cbaston@centralrecovery.com>
2018-06-01 14:02:11 -0700

    add dates to rec education and employment

--- tables/clientinfo/tbl_rec_education.js
+++ tables/clientinfo/tbl_rec_education.js
@@ -9,15 +9,26 @@ let recId = 1
 // create seed data - total is set in tables-config.
 for (let i = 1; i <= tables.tbl_rec_education.total; i++) {
 
+
+  // some dates to make dates flow correctly.
+  let AS_OF = new Date();
+  AS_OF.setDate(AS_OF.getDate() - random.number(365))
+
+  let DATE_UPD = new Date(AS_OF)
+  DATE_UPD.setDate(DATE_UPD.getDate() + 7)
+
+  let END_DATE = new Date(DATE_UPD)
+  END_DATE.setDate(END_DATE.getDate() + 7)
+
   // Many to one ids.
   if (recId > tables.tbl_recoverees.total) recId = 1
 
   seeder.push({
-    "AS_OF": random.date(),
+    "AS_OF": random.isoDateFormatTimeCreate(AS_OF),
     "COLLEGE": Math.floor(Math.random()),
-    "DATE_ADD": random.date(),
-    "DATE_UPD": random.date(),
-    "END_DATE": random.date(),
+    "DATE_ADD": random.isoDateFormatTimeCreate(AS_OF),
+    "DATE_UPD": random.isoDateFormatTimeCreate(DATE_UPD),
+    "END_DATE": random.isoDateFormatTimeCreate(END_DATE),
     "RECOVEREE_EDUCATION": random.number(tables.tbl_education.types.length),
     "RECOVEREE_EFF_DATE": random.date(),
     "RECOVEREE_ID": recId,
--- tables/employment/tbl_rec_employment.js
+++ tables/employment/tbl_rec_employment.js
@@ -9,15 +9,27 @@ let recId = 1
 // create seed data - total is set in tables-config.
 for (let i = 1; i <= tables.tbl_rec_employment.total; i++) {
 
+
+  // some dates to make dates flow correctly.
+  let AS_OF = new Date();
+  AS_OF.setDate(AS_OF.getDate() - random.number(365))
+
+  let DATE_UPD = new Date(AS_OF)
+  DATE_UPD.setDate(DATE_UPD.getDate() + 7)
+
+  let END_DATE = new Date(DATE_UPD)
+  END_DATE.setDate(END_DATE.getDate() + 7)
+
+
   // Many to one ids.
   if (recId > tables.tbl_recoverees.total) recId = 1
 
   seeder.push({
-    "AS_OF": random.date(),
-    "DATE_ADD": random.date(),
-    "DATE_UPD": random.date(),
-    "END_DATE": random.date(),
-    "RECOVEREE_EFF_DATE": random.date(),
+    "AS_OF": random.isoDateFormatTimeCreate(AS_OF),
+    "DATE_ADD": random.isoDateFormatTimeCreate(AS_OF),
+    "DATE_UPD": random.isoDateFormatTimeCreate(DATE_UPD),
+    "END_DATE": random.isoDateFormatTimeCreate(END_DATE),
+    "RECOVEREE_EFF_DATE": random.isoDateFormatTimeCreate(AS_OF),
     "RECOVEREE_EMPLOYMENT": random.number(tables.tbl_employment.types.length),
     "RECOVEREE_ID": recId,
     "USERNAME_ADD": faker.internet.userName(),
