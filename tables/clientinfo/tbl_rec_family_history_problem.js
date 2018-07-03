017b38ca82c8e59fd86b8e52aedd344a6927027d
Carlos Baston <cbaston@centralrecovery.com>
2018-07-03 12:09:22 -0700

    change name to NAME

---
+++ tables/clientinfo/tbl_rec_family_history_problem.js
@@ -0,0 +1,28 @@
+const tables = require('../../configuration/tables-config')
+const random = require('../../helpers/random')
+const faker = require('faker')
+
+// Seed data.
+const seeder = []
+let recId = 1
+
+// create seed data - total is set in tables-config.
+for (let i = 1; i <= tables.tbl_rec_institutions.total; i++) {
+
+  // Many to one ids.
+  if (recId > tables.tbl_rec_family_history_problem.total) recId = 1
+
+  seeder.push({
+    "RECOVEREE_ID": recId,
+    "ID": i,
+    "FAMILY": random.arrayOfNumbers(15)
+  })
+  recId++
+}
+
+module.exports = {
+  "seeder": seeder,
+  "indexes": ['ID'],
+  "compoundIndexes": [],
+  "table": 'TBL_REC_FAMILY_HISTORY_PROBLEMS'
+}
